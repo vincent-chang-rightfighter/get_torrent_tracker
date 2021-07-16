6ec3854c0440aa96da99ec1df6d4397a0ecdee43
VincentChang <vincent870922@gmail.com>
2021-07-16 14:03:07 +0800

    fix bug

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ got(sourcesurl).then(response => {
     console.log('statusCode:', response.statusCode);
     const dom = new JSDOM(response.body);
     const responsetext = dom.window.document.querySelector('pre').textContent;
-    const savetext = responsetext.replace(/\n/g, '\n\n');
+    const savetext = responsetext.replace(/\n/g, '\r\n\r\n');
     fs.writeFile('torrent_tracker.txt', savetext, function (err) {
         if (err) return console.log(err);
         console.log('getting torrent tracker from torrends.to');
